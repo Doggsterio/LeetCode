1a05c366b697bf58877aa3910784f3d3db2f2766
Dmitry Matov <matovxd@gmail.com>
2023-10-26 00:43:24 +0300

    add task 66

---
+++ Easy/66. Plus One.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    let rem = 1;
+    for(let i=digits.length-1; i>=0; i--) {
+        let temp = (digits[i] + rem);
+        digits[i] = temp % 10;
+        rem = Math.floor(temp / 10);
+    }
+    return (rem==0) ? digits : [rem].concat(digits); 
+};
